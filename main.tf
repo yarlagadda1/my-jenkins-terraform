2b25fdb638ed8127cf0ead1fe428380433655b5f
venkaty <yarlagadda1@gmail.com>
2023-08-28 17:41:22 -0400

    Changed name of the instance

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "ec2" {
   ami           = "ami-053b0d53c279acc90" # us-east-1
   instance_type = "t2.micro"
   tags = {
-      Name = "Terraform-Provisioned-Instance"
+      Name = "Terraform-Provisioned-Test"
   }
 }
