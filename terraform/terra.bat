bfd759d01afabbabcfdd02909fafc9c266e8a5e9
yarlagadda1 <yarlagadda1@gmail.com>
2023-08-28 20:05:28 -0400

    modifed

--- terraform/terra.bat
+++ terraform/terra.bat
@@ -1,6 +1,6 @@
- cd "C://Users//yarla//OneDrive//Desktop//DevOps//my-jenkins-terraform//"
+ cd "C://Users//yarla//OneDrive//Desktop//DevOps//my-jenkins-terraform//infastructure//"
  terraform init
- cd "C://Users//yarla//OneDrive//Desktop//DevOps//my-jenkins-terraform//"
- terraform plan -out terraform//tfplan
- cd "C://Users//yarla//OneDrive//Desktop//DevOps//my-jenkins-terraform//"
- terraform show -no-color terraform//tfplan > terraform//tfplan.txt
+ cd "C://Users//yarla//OneDrive//Desktop//DevOps//my-jenkins-terraform//infastructure//"
+ terraform plan -out ..//terraform//tfplan
+ cd "C://Users//yarla//OneDrive//Desktop//DevOps//my-jenkins-terraform//infastructure"
+ terraform show -no-color ..//terraform//tfplan > terraform//tfplan.txt
